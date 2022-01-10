c9e800ce24049066a00c5d17dea954f070a4cf72
kbujak <konradbjk@gmail.com>
2022-01-10 09:13:02 +0100

    adding about us page

---
+++ pages/about.js
@@ -0,0 +1,14 @@
+import NavigationBar from "@/components/NavigationBar";
+import PeopleList from "@/components/PeopleList";
+
+export default function About() {
+    return(
+        <>
+        <div>
+            <NavigationBar/>
+            <PeopleList/>
+            <h1>Zespol kompasu</h1>
+        </div>
+        </>
+    )
+}
